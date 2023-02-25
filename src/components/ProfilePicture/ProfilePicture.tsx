78bd40cede29c630d8449a3851c6d29440cc8dbb
Daniel Grimm <danielsgrimm@hotmail.com>
2023-02-25 10:51:36 -0600

    Minor sidebar improvements

--- src/components/ProfilePicture/ProfilePicture.tsx
+++ src/components/ProfilePicture/ProfilePicture.tsx
@@ -1,7 +1,8 @@
 import profilePicture from '../../assets/images/daniel.jpeg';
+import { Image } from './ProfilePicture.style';
 
 export const ProfilePicture: React.FC = () => {
-    return <img src={profilePicture} alt="Profile Picture" />;
+    return <Image src={profilePicture} alt="Profile Picture" />;
 }
 
 export default ProfilePicture;
--- src/components/Sidebar/Sidebar.style.tsx
+++ src/components/Sidebar/Sidebar.style.tsx
@@ -7,7 +7,6 @@ export const Bar = styled.div`
   position: absolute;
   height: 100vh;
   width: 10rem;
-  gap: 2rem;
   background-color: ${theme.emerald50};
 `;
 
@@ -15,9 +14,10 @@ export const ListItem = styled.div`
   display: flex;
   font-size: 20px;
   color: ${theme.white};
+  padding-top: 1rem;
+  padding-bottom: 1rem;
   &:hover {
-    background-color: ${theme.emerald160};
+    background-color: ${theme.emerald200};
     color: ${theme.black};
   }
 `
-
