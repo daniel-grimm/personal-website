41b068cb1dde4493db9ad01b567c16c2d400ee48
Daniel Grimm <danielsgrimm@hotmail.com>
2023-02-27 08:38:41 -0600

    Removing unused import

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Bar, IconWrapper } from './Topbar.style';
 import { FaHouseUser, FaScroll, FaTools } from 'react-icons/fa';
 import { BsFillPeopleFill } from 'react-icons/bs';
-import { theme } from '../../theme';
 
 export const Topbar: React.FC = () => {
 
