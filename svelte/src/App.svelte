427b19733b9eab8c3b02a9e6b5ccbd774cccc3d6
Daniel Grimm <danielsgrimm@hotmail.com>
2023-09-23 10:40:17 -0500

    Adding styled SVGs

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
   import Counter from './lib/Counter.svelte';
   import Experience from './lib/Experience.svelte';
+  import Links from './lib/Links.svelte';
   import { isExperience, isHome } from './store/pageStore';
   import { EXPERIENCE } from './utils/pageConstants';
   import { togglePage } from './utils/togglePage';
 </script>
-
 <main>
   {#if $isHome}
     <Experience />
@@ -16,4 +16,14 @@
   {/if}
 
   <button on:click={() => togglePage(EXPERIENCE)}>Toggle</button>
+
+  <Links />
 </main>
+
+<style>
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+</style>
