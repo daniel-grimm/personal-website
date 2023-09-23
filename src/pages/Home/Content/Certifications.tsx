89613cebcb054ade64284cf3933989ecedfe4702
Daniel Grimm <danielsgrimm@hotmail.com>
2023-09-22 19:01:25 -0500

    Adding Azure Expert Certification Badges

--- src/pages/Home/Content/Certifications.tsx
+++ src/pages/Home/Content/Certifications.tsx
@@ -3,6 +3,8 @@ import azureFundamentals from "../../../assets/images/azurefundamentals.png";
 import azureDataFundamentals from "../../../assets/images/azuredatafundamentals.png";
 import awsFundamentals from "../../../assets/images/awscloudpractitioner.png";
 import azureAdmin from "../../../assets/images/azureadministratorassociate.png";
+import azureDevops from "../../../assets/images/azuredevopsexpert.png";
+import azureArchitect from "../../../assets/images/azuresolutionsarchitect.png";
 import { Image } from '../../../components/Image/Image.style';
 
 export const Certifications: React.FC = () => {
@@ -10,6 +12,8 @@ export const Certifications: React.FC = () => {
     return (
         <>
             <ImageGallery>
+                <Image src={azureArchitect} width='120px' height='120px' />
+                <Image src={azureDevops} width='120px' height='120px' />
                 <Image src={azureAdmin} width='120px' height='120px' />
                 <Image src={azureFundamentals} width='120px' height='120px' />
                 <Image src={azureDataFundamentals} width='120px' height='120px' />
